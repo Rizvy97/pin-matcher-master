1cb3404e338612036066ec3d09b1ca86e40b56f7
Rizvyahamed <rizvyahamed97@gmail.com>
2020-08-04 21:35:08 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+//Generate Pin 
+document.getElementById('generate_clicker').addEventListener('click', function () {
+    const generateRandomNumber = Math.floor(1000 + Math.random() * 9999);
+    document.getElementById('generate_input').value = generateRandomNumber;
+});
+document.getElementById('generateSubmit').addEventListener('click', function () {
+    const generateInput = document.getElementById('generate_input').value;
+    const generateOutput = document.getElementById('generate_output').value;
+    if (generateInput === generateOutput) {
+        const submitOutputDone = document.getElementById('outputDone');
+        submitOutputDone.style.display = 'block';
+        document.getElementById('generate_input').value = ' ';
+        document.getElementById('generate_output').value = ' ';
+    }
+
+    if (generateInput !== generateOutput) {
+        const submitOutputError = document.getElementById('outputError');
+        submitOutputError.style.display = 'block';
+    }
+});
